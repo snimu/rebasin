a67e82fbaabe8a095d735ba4a882642b8c0fa7f7
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-03-29 10:21:41 +0200

    Remove `identity_tensor` from util.py

--- rebasin/util.py
+++ rebasin/util.py
@@ -3,24 +3,4 @@ from __future__ import annotations
 import torch
 
 
-def _init_identity(tensor: torch.Tensor, n_eye: int) -> torch.Tensor:
-    if len(tensor.size()) == 2:
-        return torch.eye(n_eye)
 
-    for i, tensor_ in enumerate(tensor):
-        tensor[i] = _init_identity(tensor_, n_eye)
-    return tensor
-
-
-def identity_tensor(tensor: torch.Tensor | torch.nn.Parameter) -> torch.Tensor:
-    dims = list(tensor.size())
-
-    if len(dims) < 2:
-        return torch.ones_like(tensor)
-
-    n_eye = max(dims[-1], dims[-2])
-    dims[-2], dims[-1] = n_eye, n_eye
-
-    perm = torch.empty(dims)
-    perm = _init_identity(perm, n_eye)
-    return perm
--- tests/test_util.py
+++ tests/test_util.py
@@ -2,17 +2,4 @@ from __future__ import annotations
 
 import torch
 
-from rebasin.util import identity_tensor
 
-
-def test_identity_tensor() -> None:
-    weights = (
-        torch.randn(2, 3, 4),
-        torch.randn(1, 2, 3, 4),
-        torch.randn(3, 3, 3),
-        torch.randn(2, 2)
-    )
-
-    for weight in weights:
-        identity = identity_tensor(weight)
-        assert torch.all(weight == weight @ identity)
