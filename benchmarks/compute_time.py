d667550136a4f2126f94e7845a037449060a5780
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-04-03 10:43:14 +0200

    Improve and rerun benchmarks

--- benchmarks/compute_time.py
+++ benchmarks/compute_time.py
@@ -9,6 +9,7 @@ from time import perf_counter
 from typing import Any
 
 import torch
+from torch import nn
 from torchvision.models import resnet18, resnet50, resnet101  # type: ignore[import]
 from tqdm import tqdm
 
@@ -33,70 +34,52 @@ class BenchmarkPermutationCoordinateDescent:
 
     Results on a 2019 MacBook Air (1.6 GHz Intel Core i5, 8 GB 2133 MHz LPDDR3):
 
-    - ResNet18: 0.551 seconds
-    - ResNet50: 2.013 seconds
-    - ResNet101: Exceeded maximum recursion depth... # TODO: Fix this
+    - ResNet18: 0.549 seconds
+    - ResNet50: 2.322 seconds
+    - ResNet101: 3.086 seconds
     """
     @staticmethod
-    def test_resnet18(iters: int = 100) -> None:
-        print("Benchmarking PermutationCoordinateDescent on ResNet18...")
+    def benchmark_pcd(
+            model_name: str,
+            model_a: nn.Module,
+            model_b: nn.Module,
+            input_data: Any,
+            iters: int = 100
+    ) -> None:
+        print(f"Benchmarking PermutationCoordinateDescent on {model_name}...")
         elapsed = 0.0
 
         for _ in tqdm(range(iters)):
-            model_a = resnet18()
-            model_b = resnet18()
-
             with Timer() as t:
-                pcd = PermutationCoordinateDescent(
-                    model_a, model_b, torch.randn(1, 3, 224, 224)
-                )
+                pcd = PermutationCoordinateDescent(model_a, model_b, input_data)
                 pcd.calculate_permutations()
                 pcd.apply_permutations()
 
             elapsed += t.elapsed
 
-        print(f"ResNet18: {elapsed / iters:.3f} seconds")
+        print(f"{model_name}: {elapsed / iters:.3f} seconds")
 
-    @staticmethod
-    def test_resnet50(iters: int = 100) -> None:
-        print("Benchmarking PermutationCoordinateDescent on ResNet50...")
-        elapsed = 0.0
+    @classmethod
+    def test_resnet18(cls, iters: int = 100) -> None:
+        cls.benchmark_pcd(
+            "ResNet18", resnet18(), resnet18(), torch.randn(1, 3, 224, 224), iters
+        )
 
-        for _ in tqdm(range(iters)):
-            model_a = resnet50()
-            model_b = resnet50()
+    @classmethod
+    def test_resnet50(cls, iters: int = 100) -> None:
+        cls.benchmark_pcd(
+            "ResNet50", resnet50(), resnet50(), torch.randn(1, 3, 224, 224), iters
+        )
 
-            with Timer() as t:
-                pcd = PermutationCoordinateDescent(
-                    model_a, model_b, torch.randn(1, 3, 224, 224)
-                )
-                pcd.calculate_permutations()
-                pcd.apply_permutations()
-
-            elapsed += t.elapsed
-
-        print(f"ResNet50: {elapsed / iters:.3f} seconds")
-
-    @staticmethod
-    def test_resnet101(iters: int = 100) -> None:
-        print("Benchmarking PermutationCoordinateDescent on ResNet101...")
-        elapsed = 0.0
-
-        for _ in tqdm(range(iters)):
-            model_a = resnet101()
-            model_b = resnet101()
-
-            with Timer() as t:
-                pcd = PermutationCoordinateDescent(
-                    model_a, model_b, torch.randn(1, 3, 224, 224)
-                )
-                pcd.calculate_permutations()
-                pcd.apply_permutations()
-
-            elapsed += t.elapsed
-
-        print(f"ResNet101: {elapsed / iters:.3f} seconds")
+    @classmethod
+    def test_resnet101(cls, iters: int = 100) -> None:
+        cls.benchmark_pcd(
+            "ResNet101", resnet101(), resnet101(), torch.randn(1, 3, 224, 224), iters
+        )
 
 
 if __name__ == "__main__":
-    BenchmarkPermutationCoordinateDescent.test_resnet50()
+    bench = BenchmarkPermutationCoordinateDescent()
+    bench.test_resnet18()
+    bench.test_resnet50()
+    bench.test_resnet101()
