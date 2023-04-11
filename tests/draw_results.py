bfc230a8b8dddc6de6f90c5c34d26e6ee186b2b0
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-04-11 14:46:02 +0200

    Improve plotting and reorganize results

--- tests/draw_results.py
+++ tests/draw_results.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import csv
 import os
 from collections.abc import Generator
-from typing import NamedTuple
+from typing import Any, NamedTuple
 
 import matplotlib.pyplot as plt  # type: ignore[import]
 import numpy as np
@@ -11,22 +11,24 @@ import numpy as np
 from tests.fixtures.mandw import MODELS_AND_WEIGHTS
 
 
-def full_filename(file: str) -> str:
+def full_filename(file: str, lib: str, dataset: str = "") -> str:
     filename = os.path.dirname(__file__)
     filename = os.path.join(filename, "results")
+    filename = os.path.join(filename, lib) if lib else filename
+    filename = os.path.join(filename, dataset) if dataset else filename
     filename = os.path.join(filename, file)
     return filename
 
 
-def to_named_img(file: str, name: str) -> str:
+def to_named_img(file: str, name: str, lib: str, dataset: str = "") -> str:
     filename = file.split("/")[-1]  # bla.csv
     filename = os.path.join("images", filename)
-    filename = full_filename(filename)
+    filename = full_filename(filename, lib, dataset)
     return f"{filename[:-4]}_{name}.png"
 
 
-def file_generator() -> Generator[str, None, None]:
-    for file in os.listdir(full_filename("")):  # lists files in results
+def file_generator(lib: str) -> Generator[str, None, None]:
+    for file in os.listdir(full_filename("", lib)):  # list files in results
         if file.endswith(".csv"):
             yield file
 
@@ -39,7 +41,7 @@ class SweepInfo(NamedTuple):
     losses: list[float]
 
 
-def get_info(file: str) -> tuple[SweepInfo, SweepInfo, SweepInfo]:
+def get_info(file: str, dataset: str) -> tuple[SweepInfo, SweepInfo, SweepInfo]:
     assert file.endswith(".csv")
     file = file.split("/")[-1]  # only get model name
 
@@ -53,7 +55,7 @@ def get_info(file: str) -> tuple[SweepInfo, SweepInfo, SweepInfo]:
     sweep_a_b_rebasin: list[float] = []
     sweep_b_orig_b_rebasin: list[float] = []
 
-    with open(full_filename(file)) as f_:
+    with open(full_filename(file, "torchvision", dataset)) as f_:
             reader = csv.reader(f_)
             for i, row in enumerate(reader):
                 if i == 0:
@@ -70,8 +72,8 @@ def get_info(file: str) -> tuple[SweepInfo, SweepInfo, SweepInfo]:
     )
 
 
-def bar_plot(file: str) -> None:
-    sweep_ab_orig, sweep_ab_rebasin, sweep_b_orig_b_rebasin = get_info(file)
+def bar_plot(file: str, dataset: str) -> None:
+    sweep_ab_orig, sweep_ab_rebasin, sweep_b_orig_b_rebasin = get_info(file, dataset)
 
     loss_a = sweep_ab_orig.losses[0]
     loss_b_orig = sweep_ab_orig.losses[-1]
@@ -114,13 +116,13 @@ def bar_plot(file: str) -> None:
     plt.xlim(-0.5, len(model_names) - 0.5)
 
     # Save the plot
-    filename = to_named_img(file, "bar")
-    fig.savefig(full_filename(filename), dpi=300)
+    filename = to_named_img(file, "bar", "torchvision", dataset)
+    fig.savefig(filename, dpi=300)
     # plt.show()
 
 
-def line_plot(file: str) -> None:
-    sweep_ab_orig, sweep_ab_rebasin, sweep_b_orig_b_rebasin = get_info(file)
+def line_plot(file: str, dataset: str = "cifar10") -> None:
+    sweep_ab_orig, sweep_ab_rebasin, sweep_b_orig_b_rebasin = get_info(file, dataset)
     sweeps = sweep_ab_orig, sweep_ab_rebasin, sweep_b_orig_b_rebasin
     sweep_names = ['a-b(orig)', 'a-b(rebasin)', 'b(orig)-b(rebasin)']
 
@@ -224,11 +226,129 @@ def line_plot(file: str) -> None:
     plt.xlabel('Interpolation: % w_b')
 
     # Save the plot
-    filename = to_named_img(file, "line")
-    fig.savefig(full_filename(filename), dpi=300)
+    filename = to_named_img(file, "line", "torchvision", dataset)
+    fig.savefig(filename, dpi=300)
+
+
+def read_csv(file: str) -> dict[str, list[Any]]:
+    assert file.endswith(".csv")
+    with open(file) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        data: dict[str, list[str | float | int]] = {h: [] for h in header}
+        for row in reader:
+            for h, v in zip(header, row):  # noqa: B905
+                data[h].append(float(v))
+
+    return data
+
+
+def draw_hlb_gpt() -> None:
+    """Draw the plots for the HLB-GPT model."""
+    lfile = full_filename("hlb-gpt/losses.csv", "", "")
+    afile = full_filename("hlb-gpt/accuracies.csv", "", "")
+    pfile = full_filename("hlb-gpt/perplexities.csv", "", "")
+
+    losses = read_csv(lfile)
+    accuracies = read_csv(afile)
+    perplexities = read_csv(pfile)
+
+    # Create fig & axs
+    fig, axs = plt.subplots(
+        nrows=3, ncols=1, figsize=(6, 8), sharex='all', gridspec_kw={'hspace': 0.7}
+    )
+
+    # Set titles
+    fig.suptitle("HLB-GPT", fontsize=14, fontweight='bold')
+    axs[0].set_title("Losses", loc="center", fontsize=10)
+    axs[1].set_title("Perplexities", loc="center", fontsize=10)
+    axs[2].set_title("Accuracies", loc="center", fontsize=10)
+
+    # Draw
+    draw_ax(1, axs[0], losses, "min", "Validation Loss")
+    draw_ax(2, axs[1], perplexities, "min", "Validation Perplexity")
+    draw_ax(3, axs[2], accuracies, "max", "Validation Accuracy")
+
+    # xlabel
+    plt.xlabel('Interpolation: % w_b')
+
+    # Save the plot
+    filename = to_named_img("hlb-gpt", "line", "hlb-gpt")
+    fig.savefig(filename, dpi=300)
+    # plt.show()
+
+
+def draw_ax(
+        index: int, ax: Any, data: dict[str, list[Any]], min_or_max: str, ylabel: str
+) -> None:
+    """Draw the plot for a single ax."""
+    # Define ylims
+    ymax = np.max([
+        np.max(data['ab_orig']).item(),
+        np.max(data['ab_rebasin']).item(),
+        np.max(data['b_orig_b_rebasin']).item()
+    ])
+    ymin = np.min([
+        np.min(data['ab_orig']).item(),
+        np.min(data['ab_rebasin']).item(),
+        np.min(data['b_orig_b_rebasin']).item()
+    ])
+    ylim_upper = ymax * 1.1
+    ylim_lower = ymin * 0.9
+
+    # Set scientific notation for large numbers
+    if ymax >= 100.0:
+        ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
+
+    # Set x-labels
+    xlabels = np.arange(len(data['ab_orig']), dtype=np.float64)
+    xlabels /= (float(len(data['ab_orig'])) - 1)
+    xlabels *= 100.0  # convert to percentage
+
+    # Write the min/max values
+    best_val_abo, best_idx_abo = (
+        (np.max(data['ab_orig']), np.argmax(data['ab_orig']))
+        if min_or_max == 'max'
+        else (np.min(data['ab_orig']), np.argmin(data['ab_orig']))
+    )
+    best_val_abr, best_idx_abr = (
+        (np.max(data['ab_rebasin']), np.argmax(data['ab_rebasin']))
+        if min_or_max == 'max'
+        else (np.min(data['ab_rebasin']), np.argmin(data['ab_rebasin']))
+    )
+    best_val_bobr, best_idx_bobr = (
+        (np.max(data['b_orig_b_rebasin']), np.argmax(data['b_orig_b_rebasin']))
+        if min_or_max == 'max'
+        else (np.min(data['b_orig_b_rebasin']), np.argmin(data['b_orig_b_rebasin']))
+    )
+
+    label_abo = (
+        f"ab_orig: {min_or_max}={best_val_abo:.2f} (%={int(xlabels[best_idx_abo])})"
+    )
+    label_abr = (
+        f"ab_rebasin: {min_or_max}={best_val_abr:.2f} (%={int(xlabels[best_idx_abr])})"
+    )
+    label_bobr = (
+        f"b_orig_b_rebasin: {min_or_max}={best_val_bobr:.2f}"
+        f" (%={int(xlabels[best_idx_bobr])})"
+    )
+
+    # Plot
+    ax.set_ylim(ylim_lower, ylim_upper)
+    ax.plot(xlabels, data['ab_orig'], label=label_abo, color='red')
+    ax.plot(xlabels, data['ab_rebasin'], label=label_abr, color='green')
+    ax.plot(xlabels, data['b_orig_b_rebasin'], label=label_bobr, color='blue')
+    ax.set_ylabel(ylabel)
+    ax.grid(True)
+
+    # Set legend
+    ax.legend(
+        loc='upper center',
+        bbox_to_anchor=(0.5, -0.3 if index == 3 else -0.05),
+        ncol=3,
+        fontsize=6
+    )
 
 
 if __name__ == "__main__":
-    for f in file_generator():
-        bar_plot(f)
-        line_plot(f)
+    draw_hlb_gpt()
