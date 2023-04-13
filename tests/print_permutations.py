938008237f6cc3a989b9bef5fff2e4640999dc94
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-04-13 11:03:16 +0200

    Add print_permutations.py to print info about the calculated permutations
    Already found bugs!
    
    (Yes, plural!)

---
+++ tests/print_permutations.py
@@ -0,0 +1,15 @@
+import torch
+
+from rebasin import PermutationCoordinateDescent
+from tests.fixtures.models import MLP
+
+if __name__ == '__main__':
+    model_a = MLP(25, num_layers=10)
+    model_b = MLP(25, num_layers=10)
+    x = torch.randn(25)
+    pcd = PermutationCoordinateDescent(model_a, model_b, x)
+    pcd.calculate_permutations()
+
+    print(len(pcd.permutations))
+    for permutation in pcd.permutations:
+        print(permutation)
