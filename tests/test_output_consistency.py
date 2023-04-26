e3f7ebd9a31f09ceb226f71d61868bd704bf3923
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-04-26 15:04:00 +0200

    Create test_output_consistency.py; checks how to fulfill paper's constraint
    The constraint being that the permuted model should yield the same output as the unpermuted one.

---
+++ tests/test_output_consistency.py
@@ -0,0 +1,141 @@
+"""
+The purpose of this test is to find out how to properly permute the weights such that
+the output of the model stays constant.
+
+This is for different Module-types.
+"""
+
+from __future__ import annotations
+
+import torch
+from torch import nn
+
+
+def test_model_output_consistency_tensors() -> None:
+    """
+    Demonstrate that the output of a model is invariant to the
+    permutation of the weight, iff the input is also permuted accordingly.
+    """
+    W = torch.randn(10)
+    x = torch.randn(10)
+    y_orig = x @ W
+
+    perm = torch.randperm(10)
+    W_new = W[perm]
+    x_new = x[perm]
+    y_new = x_new @ W_new
+
+    assert torch.allclose(y_orig, y_new)
+
+
+class TestConsistencyLinear:
+
+    @staticmethod
+    def test_model_output_consistency_lin_only_input() -> None:
+        """
+        Demonstrate that, as long as the output dimension is
+        untouched, permuting the input and the input-dimension of the weights
+        yields the same output as the unpermuted input and weight.
+        """
+        model = nn.Linear(10, 10)
+        x = torch.randn(10)
+        y_orig = model(x)
+
+        perm = torch.randperm(10)
+        model.weight.data = model.weight.data[:, perm]
+        y_xfail = model(x)
+        assert not torch.allclose(y_orig, y_xfail)
+
+        x_new = x[perm]
+        y_xpass = model(x_new)
+        assert torch.allclose(y_orig, y_xpass)
+
+    @staticmethod
+    def test_permuted_output_produces_same_digits() -> None:
+        model = nn.Linear(10, 10, bias=False)
+        x = torch.randn(10)
+        y_orig = model(x)
+
+        perm_out = torch.randperm(10)
+        model.weight.data = model.weight.data[perm_out]
+        y_new = model(x)
+
+        for digit in y_new:
+            assert digit in y_orig
+
+    @staticmethod
+    def test_perm_output_and_next_input_with_same_permutation() -> None:
+        for _ in range(100):
+            lin1, lin2 = nn.Linear(8, 10, bias=False), nn.Linear(10, 12, bias=False)
+            model = nn.Sequential(lin1, lin2)
+            x = torch.randn(8)
+            y_orig = model(x)
+
+            perm = torch.randperm(10)
+
+            lin1.weight.data = lin1.weight.data[perm]
+            lin2.weight.data = lin2.weight.data[:, perm]
+
+            y_new = model(x)
+            # Floating point operations lead to enough error that we need to
+            # relax the tolerance a bit.
+            assert torch.allclose(y_orig, y_new, atol=1e-7, rtol=1e-4)
+
+    @staticmethod
+    def test_perm_out_same_perm_in_multi_layer() -> None:
+        for _ in range(100):
+            lin1 = nn.Linear(8, 10, bias=False)
+            lin2 = nn.Linear(10, 12, bias=False)
+            lin3 = nn.Linear(12, 14, bias=False)
+
+            model = nn.Sequential(lin1, lin2, lin3)
+            x = torch.randn(8)
+            y_orig = model(x)
+
+            perm1 = torch.randperm(10)
+            perm2 = torch.randperm(12)
+
+            lin1.weight.data = lin1.weight.data[perm1]  # permute output dim of lin1
+            lin2.weight.data = lin2.weight.data[:, perm1]  # reverse effects of perm1
+            lin2.weight.data = lin2.weight.data[perm2]  # permute output dim of lin2
+            lin3.weight.data = lin3.weight.data[:, perm2]  # reverse effects of perm2
+
+            y_new = model(x)
+
+            assert torch.allclose(y_orig, y_new, atol=1e-7, rtol=1e-4)
+
+
+class TestConsistencyConv2d:
+
+    @staticmethod
+    def test_output_dim_perm_is_output_perm() -> None:
+        conv = nn.Conv2d(3, 2, (3, 3), bias=False)
+        x = torch.randn(1, 3, 9, 9)
+        y_orig = conv(x)
+
+        perm = torch.tensor([1, 0])
+        conv.weight.data = conv.weight.data[perm]
+        y_new = conv(x)
+
+        assert torch.allclose(y_orig, y_new[:, perm])
+
+    @staticmethod
+    def test_output_perm_multi_layer() -> None:
+        for _ in range(10):
+            # conv1.weight.shape == [10, 3, 3, 3]
+            conv1 = nn.Conv2d(3, 10, (3, 3), bias=False)
+            # conv2.weight.shape == [12, 10, 3, 3]
+            conv2 = nn.Conv2d(10, 12, (3, 3), bias=False)
+            relu = nn.ReLU()
+
+            model = nn.Sequential(conv1, relu, conv2)
+            x = torch.randn(4, 3, 9, 9)
+            y_orig = model(x)
+
+            perm = torch.randperm(10)
+            conv1.weight.data = conv1.weight.data[perm]
+            conv2.weight.data = conv2.weight.data[:, perm]
+
+            y_new = model(x)
+
+            assert torch.allclose(y_orig, y_new, atol=1e-7, rtol=1e-4)
