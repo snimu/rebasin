32040983b01efa826312d30adaaae20177035aa7
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2023-05-17 14:19:28 +0200

    Automatically install requirements upon installation

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+setup(
+    install_requires=[
+        "torch",
+        "numpy",
+        "scipy",
+        "tqdm",
+        "graphviz",
+        "torchview",
+    ]
+)
